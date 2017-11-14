2689c027f4760fd44a80047da9b447ac26860c5a
TonyApD <t.lopar@student.han.nl>
2017-11-14 12:15:21 +0100

    ghj

--- src/main/java/Kruskal.java
+++ src/main/java/Kruskal.java
@@ -45,11 +45,14 @@ public class Kruskal {
                 }
 
             } else if (mstEdges.size() == nodeCount - 1) {
+                for (Edge e : possibleEdges) {
+                    System.out.println(e);
+                }
                 if (!possibleEdges.isEmpty()) {
                     mstEdges.add(possibleEdges.get(0));
                     nodeSet.union(possibleEdges.get(0).getVertex1(), possibleEdges.get(0).getVertex2());
                 } else {
-                    mstEdges.add(currentEdge);
+                    if (tryToFindSubpaths(mstEdges, currentEdge)) break;
                 }
             }
         }
@@ -60,5 +63,71 @@ public class Kruskal {
         System.out.print(outputMessage);
 
         return mstEdges;
+    }
+
+    private boolean tryToFindSubpaths(ArrayList<Edge> mstEdges, Edge currentEdge) {
+        List<Edge> pathsFromVertex1 = findEdgesFromVertex(mstEdges, currentEdge.getVertex1());
+        List<Edge> pathsFromVertex2 = findEdgesFromVertex(mstEdges, currentEdge.getVertex2());
+
+        ArrayList<Edge> intersect = (ArrayList<Edge>) getListIntersection(pathsFromVertex1, pathsFromVertex2);
+        if (!intersect.isEmpty()) {
+            if (findEdge(mstEdges, intersect.get(0), currentEdge).getWeight() + findEdge(mstEdges,
+                    intersect.get(0), currentEdge).getWeight() == currentEdge.getWeight()) {
+                //abort don't add
+                return true;
+            }
+        } else {
+            if (thereIsAShorterPathPossible(currentEdge.getWeight(), pathsFromVertex1, pathsFromVertex2)) {
+                //try to find a common vertex from a vertex in a level deeper
+                //tryToFindSubpaths(mstEdges, pathsFromVertex1.get(0));
+            } else {
+                //insert the node since there cannot be two subpaths with a lower or same weight
+                mstEdges.add(currentEdge);
+            }
+        }
+        return false;
+    }
+
+    private boolean thereIsAShorterPathPossible(int currentWeight, List<Edge> pathsFromVertex1, List<Edge> pathsFromVertex2) {
+        for (Edge e1 : pathsFromVertex1) {
+            for (Edge e2 : pathsFromVertex2) {
+                if (e1.getWeight() + e2.getWeight() < currentWeight) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private List<Edge> findEdgesFromVertex(List<Edge> mstEdges, int vertex) {
+        List<Edge> returnList = new ArrayList<>();
+        for (Edge e : mstEdges) {
+            if (e.getVertex1() == vertex || e.getVertex2() == vertex) {
+                returnList.add(e);
+            }
+        }
+        return returnList;
+    }
+
+    private Edge findEdge(List<Edge> mstEdges, Edge edge, Edge vertex2) {
+        for (Edge e : mstEdges) {
+            if ((e.getVertex1() == edge.getVertex1() && e.getVertex2() == vertex2.getVertex2()) ||
+                    (e.getVertex1() == edge.getVertex2() && e.getVertex2() == vertex2.getVertex1())) {
+                return e;
+            }
+        }
+        return null;
+    }
+
+    private List<Edge> getListIntersection(List<Edge> list1, List<Edge> list2) {
+        List list = new ArrayList();
+
+        for (Edge t : list1) {
+            if (list2.contains(t)) {
+                list.add(t);
+            }
+        }
+
+        return list;
     }
 }
